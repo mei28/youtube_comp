d373aab308335960061b1f32554227825e182a76
楊明哲 <mingzhe.yang@hcomp.cs.tsukuba.ac.jp>
2020-05-03 11:11:14 +0900

    add plot_importance

--- models/lgbm.py
+++ models/lgbm.py
@@ -4,6 +4,7 @@ import logging
 
 from logs.logger import log_evaluation
 
+import matplotlib.pyplot as plt 
 
 def train_and_predict(X_train, X_valid, y_train, y_valid, X_test, lgbm_params):
     # データセットの作成
@@ -20,11 +21,12 @@ def train_and_predict(X_train, X_valid, y_train, y_valid, X_test, lgbm_params):
         params=lgbm_params,
         train_set=lgb_train,
         valid_sets=lgb_eval,
-        num_boost_round=1000,
+        num_boost_round=8000,
         early_stopping_rounds=10,
         callbacks=callbacks
     )
 
     y_pred = model.predict(X_test, num_iteration=model.best_iteration)
-
+    lgb.plot_importance(model, importance_type="gain", max_num_features=20,figsize=(12,6))
+    plt.show()
     return y_pred, model
